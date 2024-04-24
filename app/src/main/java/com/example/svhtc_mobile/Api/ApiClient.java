bc41400fea5e0cbf50abf36920b45e64d53fdbb4
Tan11-22 <tanwithwork@gmail.com>
2024-04-24 14:48:03 +0700

    commit 1

---
+++ app/src/main/java/com/example/svhtc_mobile/Api/ApiClient.java
@@ -0,0 +1,35 @@
+package com.example.svhtc_mobile.Api;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+    private static final String BASE_URL = "http://10.251.12.34:8080/";
+    private static Gson gson = new GsonBuilder().create();
+    public static Retrofit getClient(String authToken) {
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request newRequest  = chain.request().newBuilder()
+                        .addHeader("Authorization", "Bearer " + authToken)
+                        .build();
+                return chain.proceed(newRequest);
+            }
+        }).build();
+
+        return new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(client)
+                .build();
+    }
+}
---
+++ app/src/main/java/com/example/svhtc_mobile/Api/apiService/ILoginService.java
@@ -0,0 +1,42 @@
+package com.example.svhtc_mobile.Api.apiService;
+
+import com.example.svhtc_mobile.Model.Khoa;
+import com.example.svhtc_mobile.Model.Nganh;
+import com.google.gson.JsonObject;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Query;
+
+public interface ILoginService {
+    @POST("api/auth/user/login")
+    Call<JsonObject> login(@Query("username") String username,
+                           @Query("password") String password);
+
+    @GET("api/auth/info")
+    Call<JsonObject> getInfo(@Query("username") String username);
+
+    @GET("api/auth/danh-sach-khoa")
+    Call<List<Khoa>> getDanhSachKhoa();
+
+    @GET("api/auth/danh-sach-nganh")
+    Call<List<Nganh>> getDanhSachNganh();
+
+    @PUT("api/auth/add-khoa")
+    Call<Boolean> addKhoa(@Query("ma") String ma,
+                          @Query("ten") String ten,
+                          @Query("id") int id);
+
+    @PUT("api/auth/edit-khoa")
+    Call<Boolean> editKhoa(@Query("ma") String ma,
+                           @Query("ten") String ten,
+                           @Query("id") int id);
+
+    @PUT("api/auth/delete-khoa")
+    Call<Boolean> deleteKhoa(@Query("makhoa") String ma
+    );
+}
